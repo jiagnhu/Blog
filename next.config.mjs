ca5f17bda333a02323da0b661a930e6530873dd7
Kai <30381575+jiagnhu@users.noreply.github.com>
2025-10-16 22:21:53 +0800

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  // 确保静态资源可以被正确访问
+  poweredByHeader: false,
+  // 如果你的网站部署在子目录，可以设置 basePath
+  // basePath: '',
+  // 如果需要从 CDN 加载静态资源，可以设置 assetPrefix
+  // assetPrefix: 'https://tyk.tangyikai.top',
+};
 
 export default nextConfig;
