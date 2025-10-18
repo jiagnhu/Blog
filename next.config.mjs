0607fa892dac949d49028d0c52e2a055ac74cc54
Kai <30381575+jiagnhu@users.noreply.github.com>
2025-10-19 00:55:18 +0800

    增加时间戳

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+// @ts-ignore
+const buildTimestamp = Date.now()
+
 const nextConfig = {
   output: 'standalone',
   // 确保静态资源可以被正确访问
@@ -7,6 +10,15 @@ const nextConfig = {
   // basePath: '',
   // 如果需要从 CDN 加载静态资源，可以设置 assetPrefix
   // assetPrefix: 'https://tyk.tangyikai.top',
+  webpack(config, { dev, isServer }) {
+    if (!dev && !isServer) {
+      const timestampSuffix = `.${buildTimestamp}`
+      // 为客户端产物增加时间戳，便于区分部署版本
+      config.output.filename = `static/chunks/[name].[contenthash]${timestampSuffix}.js`
+      config.output.chunkFilename = `static/chunks/[name].[contenthash]${timestampSuffix}.js`
+    }
+    return config
+  },
 };
 
 export default nextConfig;
